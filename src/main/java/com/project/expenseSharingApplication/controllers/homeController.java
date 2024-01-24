6af5d842eceae357af61f896cbe67373ca4cc198
MuhtasimMahmud <muhtasim.mzs16@gmail.com>
2024-01-24 11:03:25 +0600

    Updated

--- src/main/java/com/project/expenseSharingApplication/controllers/homeController.java
+++ src/main/java/com/project/expenseSharingApplication/controllers/homeController.java
@@ -1,14 +1,22 @@
 package com.project.expenseSharingApplication.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class homeController {
 
     @GetMapping("/home")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("Title","Home");
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(Model model){
+        model.addAttribute("Title", "About");
+        return "about";
     }
 
 
